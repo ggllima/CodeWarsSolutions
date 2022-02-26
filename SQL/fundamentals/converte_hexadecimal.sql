ccee3bb2a015e49fc1471d0ca3ce3cbf847518cf
ggllima <ggllima@usp.br>
2022-02-26 00:23:22 -0300

    sql question

---
+++ SQL/fundamentals/converte_hexadecimal.sql
@@ -0,0 +1,15 @@
+-- to hexYou have access to a table of monsters as follows:
+
+-- monsters table schema
+-- id
+-- name
+-- legs
+-- arms
+-- characteristics
+-- Your task is to turn the numeric columns (arms, legs) into equivalent hexadecimal values.
+
+-- output table schema
+-- legs
+-- arms
+
+SELECT TO_HEX(arms) AS arms, TO_HEX(legs) AS legs FROM monsters 
